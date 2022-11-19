d7f2223e884938dfc8b5c4370ddd81c44eed8163
GIGABYTE <eo.ursuz@gmail.com>
2022-11-20 00:57:34 +0300

    Fixes

--- src/test/java/FilterTest.java
+++ src/test/java/FilterTest.java
@@ -1,15 +1,20 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilterTest {
 
@@ -45,7 +50,7 @@ public class FilterTest {
                 pricesArray.add(Double.valueOf(regularPricesArray.get(i)));
             }
         }
-        Assert.assertEquals(pricesArray,pricesArray.stream().sorted().toList());
+        Assert.assertEquals(pricesArray,pricesArray.stream().sorted().collect(Collectors.toList()));
 
     }
 
